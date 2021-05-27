c80330bdb62ad1dc2e27ee05cf600c05be098650
Simon Wörpel <simon.woerpel@medienrevolte.de>
2021-05-27 15:35:43 +0200

    apply black ; refactor stages logic a bit

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='memorious-sehrgutachten',
-    version='0.2',
+    name="memorious-sehrgutachten",
+    version="0.3",
     classifiers=[],
-    keywords='',
-    packages=find_packages('src'),
-    package_dir={'': 'src'},
+    keywords="",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
     namespace_packages=[],
     include_package_data=True,
     zip_safe=False,
-    install_requires=['memorious'],
+    install_requires=["memorious", "furl"],
 )
--- src/sehrgutachten.py
+++ src/sehrgutachten.py
@@ -2,66 +2,45 @@
 
 import os
 import re
-
-from urllib.parse import urljoin, urlparse
 from datetime import datetime
+from urllib.parse import urljoin
 
-from alephclient.api import AlephAPI
-from followthemoney import model
-from memorious.helpers import make_id
-from normality import normalize
+from furl import furl
 
-
-DEFAULT_URL = 'https://www.bundestag.de/ajax/filterlist/de/dokumente/ausarbeitungen/474644-474644/'
-PER_PAGE = 10
 MONTHS = (
-    'januar',
-    'februar',
-    'märz',
-    'april',
-    'mai',
-    'juni',
-    'juli',
-    'august',
-    'september',
-    'oktober',
-    'november',
-    'dezember'
+    "januar",
+    "februar",
+    "märz",
+    "april",
+    "mai",
+    "juni",
+    "juli",
+    "august",
+    "september",
+    "oktober",
+    "november",
+    "dezember",
 )
 WD_NAMES = {
-    'wd1': 'Geschichte, Zeitgeschichte und Politik',
-    'wd2': 'Auswärtiges, Völkerrecht, Wirtschaftliche Zusammenarbeit und Entwicklung, Verteidigung, Menschenrechte und humanitäre Hilfe',  # noqa
-    'wd3': 'Verfassung und Verwaltung',
-    'wd4': 'Haushalt und Finanzen',
-    'wd5': 'Wirtschaft und Technologie, Ernährung, Landwirtschaft und Verbraucherschutz, Tourismus',
-    'wd6': 'Arbeit und Soziales',
-    'wd7': 'Zivil-, Straf- und Verfahrensrecht, Umweltschutzrecht, Verkehr, Bau und Stadtentwicklung',
-    'wd8': 'Umwelt, Naturschutz, Reaktorsicherheit, Bildung und Forschung',
-    'wd9': 'Gesundheit, Familie, Senioren, Frauen und Jugend',
-    'wd10': 'Kultur, Medien und Sport',
-    'pe6': 'Europa'
+    "wd1": "Geschichte, Zeitgeschichte und Politik",
+    "wd2": "Auswärtiges, Völkerrecht, Wirtschaftliche Zusammenarbeit und Entwicklung, Verteidigung, Menschenrechte und humanitäre Hilfe",  # noqa
+    "wd3": "Verfassung und Verwaltung",
+    "wd4": "Haushalt und Finanzen",
+    "wd5": "Wirtschaft und Technologie, Ernährung, Landwirtschaft und Verbraucherschutz, Tourismus",  # noqa
+    "wd6": "Arbeit und Soziales",
+    "wd7": "Zivil-, Straf- und Verfahrensrecht, Umweltschutzrecht, Verkehr, Bau und Stadtentwicklung",  # noqa
+    "wd8": "Umwelt, Naturschutz, Reaktorsicherheit, Bildung und Forschung",
+    "wd9": "Gesundheit, Familie, Senioren, Frauen und Jugend",
+    "wd10": "Kultur, Medien und Sport",
+    "pe6": "Europa",
 }
-META = (
-    'url',
-    'source_url',
-    'foreign_id',
-    'title',
-    'author',
-    'file_name',
-    'modified_at',
-    'published_at'
-)
-
-
-def _make_url_key(url):
-    return make_id(normalize(url))
 
 
 def _xp(html, path):
     part = html.xpath(path)
     if isinstance(part, list) and part:
         part = part[0]
-    if hasattr(part, 'text'):
+    if hasattr(part, "text"):
         part = part.text
     if isinstance(part, str):
         return part.strip()
@@ -69,24 +48,15 @@ def _xp(html, path):
 
 
 def _clean_date(value):
-    value = value.lower().replace(' ', '')
+    value = value.lower().replace(" ", "")
     for i, month in enumerate(MONTHS):
         if month in value:
-            value = value.replace(month, '%s.' % str(i + 1).zfill(2))
-            return datetime.strptime(value, '%d.%m.%Y').date().isoformat()
+            value = value.replace(month, "%s." % str(i + 1).zfill(2))
+            return datetime.strptime(value, "%d.%m.%Y").date().isoformat()
 
 
-def init(context, data):
-    store = context.datastore['sehrgutachten']
-    url = context.get('url', DEFAULT_URL)
-    host = '{0}://{1}'.format(*urlparse(url))
-    per_page = context.get('per_page', PER_PAGE)
-    page = data.get('page', 1)
-
-    res = context.http.get(url, params={
-        'limit': per_page,
-        'offset': page * per_page if page > 1 else 0
-    })
+def parse(context, data):
+    res = context.http.rehash(data)
 
     rows = res.html.xpath('//table[@class="table bt-table-data"]/tbody/tr')
 
@@ -95,65 +65,51 @@ def init(context, data):
         if not path:
             continue
 
-        url = urljoin(host, path)
-        key = _make_url_key(url)
-        if store.find_one(key=key):
-            continue
+        url = urljoin(data["url"], path)
 
         try:
             title = _xp(row, ".//div[@class='bt-documents-description']/p/strong")
-            file_name = url.split('/')[-1]
+            file_name = url.split("/")[-1]
             _, ext = os.path.splitext(file_name)
-            data = {
-                'url': url,
-                'key': key,
-                'title': title,
-                'file_name': title + ext if title else file_name,
-                'published_at': _clean_date(_xp(row, './td[@data-th="Veröffentlichung"]/p')),
-                'keywords': _xp(row, './td[@data-th="Thema"]/p'),
-                'category': _xp(row, './td[@data-th="Dokumenttyp"]/p'),
-                'mime_type': 'application/pdf',
-                'source_url': url,
-                'publisher': 'Wissenschaftliche Dienste des Deutschen Bundestages',
-                'publisher_url': 'https://www.bundestag.de/ausarbeitungen/'
+            detail_data = {
+                "url": url,
+                "title": title,
+                "file_name": title + ext if title else file_name,
+                "published_at": _clean_date(
+                    _xp(row, './td[@data-th="Veröffentlichung"]/p')
+                ),
+                "keywords": _xp(row, './td[@data-th="Thema"]/p'),
+                "category": _xp(row, './td[@data-th="Dokumenttyp"]/p'),
+                "source_url": url,
+                "publisher": "Wissenschaftliche Dienste des Deutschen Bundestages",
+                "publisher_url": "https://www.bundestag.de/ausarbeitungen/",
             }
 
-            wd_match = re.match(r'(?P<wd>wd|pe)\s*(?P<wd_id>\d+).*(?P<doc_id>\d+\/\d+)', data['title'], re.IGNORECASE)
+            wd_match = re.match(
+                r"(?P<wd>wd|pe)\s*(?P<wd_id>\d+)[\s-]+(?P<doc_id>\d+\/\d+)",
+                detail_data["title"],
+                re.IGNORECASE,
+            )
+
             if wd_match:
-                wd_id = wd_match.group('wd').lower() + wd_match.group('wd_id')
+                wd_id = wd_match.group("wd").lower() + wd_match.group("wd_id")
                 wd_id_nice = f"{wd_match.group('wd')} {wd_match.group('wd_id')}"
                 wd_name = WD_NAMES.get(wd_id, wd_id_nice)
-                data['publisher'] = f'Wissenschaftlicher Dienst "{wd_id_nice}: {wd_name}" des Deutschen Bundestages'  # noqa
-                data['publisher_url'] = f'https://www.bundestag.de/dokumente/analysen/{wd_id}'
-                data['foreign_id'] = '-'.join((wd_id, wd_match.group('doc_id')))
+                detail_data[
+                    "publisher"
+                ] = f'Wissenschaftlicher Dienst "{wd_id_nice}: {wd_name}" des Deutschen Bundestages'  # noqa
+                detail_data[
+                    "publisher_url"
+                ] = f"https://www.bundestag.de/dokumente/analysen/{wd_id}"
+                detail_data["foreign_id"] = "-".join((wd_id, wd_match.group("doc_id")))
 
-            store.insert(data)
-            context.emit(data={k: v for k, v in data.items() if k in META and v})
+            context.emit("download", data=detail_data)
 
         except Exception as e:
             context.log.error(f"Error at `{url}`: {e}")
 
+    # pagination
     if len(rows):
-        context.recurse(data={'page': page + 1})
-
-
-def enrich(context, data):
-    store = context.datastore['sehrgutachten']
-    enrich_data = store.find_one(key=_make_url_key(data['url']))
-    if enrich_data:
-        api = AlephAPI()
-        document = data['aleph_id']
-        collection = data['aleph_collection_id']
-        publisher = model.make_entity('PublicBody')
-        publisher.make_id(enrich_data['publisher'])
-        publisher.add('name', enrich_data['publisher'])
-        publisher.add('website', enrich_data['publisher_url'])
-        publisher.add('country', 'Germany')
-
-        link = model.make_entity('UnknownLink')
-        link.make_id('link', publisher.id, document)
-        link.add('subject', document)
-        link.add('object', publisher)
-        link.add('role', 'publisher')
-
-        api.write_entities(collection, [publisher.to_dict(), link.to_dict()])
+        fu = furl(data["url"])
+        fu.args["offset"] = int(fu.args["limit"]) + int(fu.args.get("offset", 0))
+        context.emit("fetch", data={"url": fu.url})
