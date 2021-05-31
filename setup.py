df5b94091c03ea784173eda6db5f733080d51fd3
Simon Wörpel <simon.woerpel@medienrevolte.de>
2021-05-31 13:56:09 +0200

    Add cron workflow

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="memorious-sehrgutachten",
-    version="0.4.1",
+    version="0.4.2",
     classifiers=[],
     keywords="",
     packages=find_packages("src"),
